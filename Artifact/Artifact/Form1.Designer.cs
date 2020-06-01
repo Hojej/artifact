162b3f5a33e00ea6fabb368a13e1f0f70c90de32
Hojej <hoojej@gmail.com>
2020-06-01 22:37:41 +0200

    Multiple clients - Not finished

--- Artifact/Artifact/Form1.Designer.cs
+++ Artifact/Artifact/Form1.Designer.cs
@@ -28,33 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.startbtn = new System.Windows.Forms.Button();
             this.btnLoadCPI = new System.Windows.Forms.Button();
-            this.IPInput = new System.Windows.Forms.TextBox();
+            this.tcphost = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.PortInput = new System.Windows.Forms.TextBox();
+            this.tcpport = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.disconnectbtn = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pbairplane = new System.Windows.Forms.PictureBox();
             this.Picbox = new System.Windows.Forms.PictureBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.udpCport = new System.Windows.Forms.TextBox();
+            this.udpcln = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.udpsHport = new System.Windows.Forms.TextBox();
+            this.udphost = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.lblTest = new System.Windows.Forms.Label();
-            this.pbairplane = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbairplane)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Picbox)).BeginInit();
             this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbairplane)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // startbtn
             // 
-            this.button1.Location = new System.Drawing.Point(12, 604);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(257, 47);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Start Artifact";
-            this.button1.UseVisualStyleBackColor = true;
+            this.startbtn.Location = new System.Drawing.Point(12, 604);
+            this.startbtn.Name = "startbtn";
+            this.startbtn.Size = new System.Drawing.Size(257, 47);
+            this.startbtn.TabIndex = 0;
+            this.startbtn.Text = "Start Artifact";
+            this.startbtn.UseVisualStyleBackColor = true;
+            this.startbtn.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnLoadCPI
             // 
@@ -66,37 +75,37 @@
             this.btnLoadCPI.UseVisualStyleBackColor = true;
             this.btnLoadCPI.Click += new System.EventHandler(this.btnLoadCPI_Click);
             // 
-            // IPInput
+            // tcphost
             // 
-            this.IPInput.Location = new System.Drawing.Point(81, 19);
-            this.IPInput.Name = "IPInput";
-            this.IPInput.Size = new System.Drawing.Size(100, 20);
-            this.IPInput.TabIndex = 2;
+            this.tcphost.Location = new System.Drawing.Point(58, 19);
+            this.tcphost.Name = "tcphost";
+            this.tcphost.Size = new System.Drawing.Size(100, 20);
+            this.tcphost.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(8, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "IP Adress:";
+            this.label1.Text = "TCP-Host";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(8, 55);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.Size = new System.Drawing.Size(50, 13);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Port number:";
+            this.label2.Text = "TCP-Port";
             // 
-            // PortInput
+            // tcpport
             // 
-            this.PortInput.Location = new System.Drawing.Point(81, 52);
-            this.PortInput.Name = "PortInput";
-            this.PortInput.Size = new System.Drawing.Size(100, 20);
-            this.PortInput.TabIndex = 4;
+            this.tcpport.Location = new System.Drawing.Point(58, 52);
+            this.tcpport.Name = "tcpport";
+            this.tcpport.Size = new System.Drawing.Size(100, 20);
+            this.tcpport.TabIndex = 4;
             // 
             // button3
             // 
@@ -108,14 +117,14 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.btnShowHide);
             // 
-            // button4
+            // disconnectbtn
             // 
-            this.button4.Location = new System.Drawing.Point(275, 554);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(257, 44);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "Disconnect";
-            this.button4.UseVisualStyleBackColor = true;
+            this.disconnectbtn.Location = new System.Drawing.Point(275, 554);
+            this.disconnectbtn.Name = "disconnectbtn";
+            this.disconnectbtn.Size = new System.Drawing.Size(257, 44);
+            this.disconnectbtn.TabIndex = 7;
+            this.disconnectbtn.Text = "Disconnect";
+            this.disconnectbtn.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
@@ -128,6 +137,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "View";
             // 
+            // pbairplane
+            // 
+            this.pbairplane.Location = new System.Drawing.Point(205, 267);
+            this.pbairplane.Name = "pbairplane";
+            this.pbairplane.Size = new System.Drawing.Size(345, 135);
+            this.pbairplane.TabIndex = 1;
+            this.pbairplane.TabStop = false;
+            // 
             // Picbox
             // 
             this.Picbox.Location = new System.Drawing.Point(3, 16);
@@ -138,17 +155,89 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.udpCport);
+            this.groupBox2.Controls.Add(this.udpcln);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.udpsHport);
+            this.groupBox2.Controls.Add(this.udphost);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Controls.Add(this.lblTest);
             this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Controls.Add(this.IPInput);
+            this.groupBox2.Controls.Add(this.tcphost);
             this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Controls.Add(this.PortInput);
+            this.groupBox2.Controls.Add(this.tcpport);
             this.groupBox2.Location = new System.Drawing.Point(13, 13);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(519, 519);
             this.groupBox2.TabIndex = 9;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Input";
+            this.groupBox2.Text = "INPUT";
+            // 
+            // udpCport
+            // 
+            this.udpCport.Location = new System.Drawing.Point(413, 52);
+            this.udpCport.Name = "udpCport";
+            this.udpCport.Size = new System.Drawing.Size(100, 20);
+            this.udpCport.TabIndex = 14;
+            // 
+            // udpcln
+            // 
+            this.udpcln.Location = new System.Drawing.Point(413, 19);
+            this.udpcln.Name = "udpcln";
+            this.udpcln.Size = new System.Drawing.Size(100, 20);
+            this.udpcln.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(361, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "UDP-Port";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(361, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "UDP-Cln";
+            // 
+            // udpsHport
+            // 
+            this.udpsHport.Location = new System.Drawing.Point(228, 52);
+            this.udpsHport.Name = "udpsHport";
+            this.udpsHport.Size = new System.Drawing.Size(100, 20);
+            this.udpsHport.TabIndex = 10;
+            // 
+            // udphost
+            // 
+            this.udphost.Location = new System.Drawing.Point(228, 19);
+            this.udphost.Name = "udphost";
+            this.udphost.Size = new System.Drawing.Size(100, 20);
+            this.udphost.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(176, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "UDP-Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(176, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "UDP-Host";
             // 
             // lblTest
             // 
@@ -159,14 +248,6 @@
             this.lblTest.TabIndex = 6;
             this.lblTest.Text = "Output";
             // 
-            // pbairplane
-            // 
-            this.pbairplane.Location = new System.Drawing.Point(205, 267);
-            this.pbairplane.Name = "pbairplane";
-            this.pbairplane.Size = new System.Drawing.Size(345, 135);
-            this.pbairplane.TabIndex = 1;
-            this.pbairplane.TabStop = false;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -174,36 +255,67 @@
             this.ClientSize = new System.Drawing.Size(1259, 663);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.disconnectbtn);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.btnLoadCPI);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.startbtn);
             this.Name = "Form1";
             this.Text = "Radar Artifact";
             this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbairplane)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Picbox)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbairplane)).EndInit();
             this.ResumeLayout(false);
 
         }
 
+        public void InActivateButton(int caseSwitch)
+        {
+            System.Windows.Forms.Button btn;
+            switch (caseSwitch)
+            {
+                case 1:
+                    btn = startbtn;
+                    break;
+                case 2:
+                    btn = disconnectbtn;
+                    break;
+                default:
+                    btn = btnLoadCPI;
+                    break;
+            }
+            btn.Enabled = false;
+        }
+
+        public void ActivateButton(System.Windows.Forms.Button btn)
+        {
+            btn.Enabled = true;
+        }
+
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button startbtn;
         private System.Windows.Forms.Button btnLoadCPI;
-        private System.Windows.Forms.TextBox IPInput;
+        private System.Windows.Forms.TextBox tcphost;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox PortInput;
+        private System.Windows.Forms.TextBox tcpport;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button disconnectbtn;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label lblTest;
         private System.Windows.Forms.PictureBox Picbox;
         private System.Windows.Forms.PictureBox pbairplane;
+        private System.Windows.Forms.TextBox udpCport;
+        private System.Windows.Forms.TextBox udpcln;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox udpsHport;
+        private System.Windows.Forms.TextBox udphost;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Artifact/Artifact/Form1.cs
+++ Artifact/Artifact/Form1.cs
@@ -8,10 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+using System.Threading;
+using System.Net.Sockets;
+using System.Text;
+
 namespace Artifact
 {
     public partial class Form1 : Form
     {
+        private int tcpPort;
+        private String tcpHost;
+
+        public TcpListener serverSocket;
+        public TcpClient clientSocket;
+
+
+
         private string selectedFileName = "";
         private bool displayPic = false;
 
@@ -20,16 +33,19 @@ namespace Artifact
             InitializeComponent();
             init();
             pbairplane.Hide();
+
         }
 
         private void init()
         {
             Picbox.Controls.Add(pbairplane);
-            pbairplane.Image = Image.FromFile("C:\\Users\\hooje\\Documents\\repos\\artifact\\Artifact\\Artifact\\Airplane.png");
+            pbairplane.Image = Image.FromFile("C:\\Users\\hooje\\Desktop\\New folder\\artifact-master\\artifact-master\\Artifact\\Artifact\\Airplane.png");
             pbairplane.SizeMode = PictureBoxSizeMode.AutoSize;
             pbairplane.Location = new Point(200, 200);
             pbairplane.BackColor = Color.Transparent;
         }
+
+
 
         private void btnLoadCPI_Click(object sender, EventArgs e)
         {
@@ -45,7 +61,7 @@ namespace Artifact
                 selectedFileName = openFileDialog1.FileName;
                 lblTest.Name = selectedFileName;
                 lblTest.Text = selectedFileName;
-                Picbox.Image = Image.FromFile("C:\\Users\\hooje\\Documents\\repos\\artifact\\Artifact\\Artifact\\gate.jpg");
+                Picbox.Image = Image.FromFile("C:\\Users\\hooje\\Desktop\\New folder\\artifact-master\\artifact-master\\Artifact\\Artifact\\gate.jpg");
             }
         }
 
@@ -60,6 +76,60 @@ namespace Artifact
                 pbairplane.Hide();
             }
             displayPic = !displayPic;
+        }
+
+
+        //start artefact
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //initTCPServer();
+            InActivateButton(1);
+        }
+
+        private void initTCPServer()
+        {
+            /*serverSocket = new TcpListener(8122);
+            clientSocket = default(TcpClient);
+            serverSocket.Start();
+            Console.WriteLine(" >> " + "Server Started");
+            clientSocket = serverSocket.AcceptTcpClient();
+            doChat();*/
+        }
+
+        private void doChat()
+        {
+            string clNo = "123";
+            int requestCount = 0;
+            byte[] bytesFrom = new byte[10025];
+            string dataFromClient = null;
+            Byte[] sendBytes = null;
+            string serverResponse = null;
+            string rCount = null;
+            requestCount = 0;
+
+            while ((true))
+            {
+                try
+                {
+                    requestCount = requestCount + 1;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    Console.WriteLine(" >> " + "From client-" + clNo + dataFromClient);
+
+                    rCount = Convert.ToString(requestCount);
+                    serverResponse = "Server to clinet(" + clNo + ") " + rCount;
+                    sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                    networkStream.Flush();
+                    Console.WriteLine(" >> " + serverResponse);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(" >> " + ex.ToString());
+                }
+            }
         }
     }
 }
